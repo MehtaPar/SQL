261417b877ba644e32f27b4905ae4a2536dccb48
Jake Willison <jakewillison23@gmail.com>
2021-05-29 00:25:50 -0400

    Update AppDelTable.sql
    Changed the column names to better fit a later example problem. File still has the commands to create and alter a table.

--- AppDelTable.sql
+++ AppDelTable.sql
@@ -2,16 +2,16 @@ CREATE DATABASE game_company;
 USE game_company;
 CREATE TABLE games (
 	id INT NOT NULL AUTO_INCREMENT,
-	rpg_column VARCHAR(255) NOT NULL,
-    fps_column VARCHAR(255) NOT NULL,
-    racing_column VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
+	name_column VARCHAR(255) NOT NULL,
+	type_column VARCHAR(255) NOT NULL,
+	release_year INT,
+	PRIMARY KEY(id)
 );
 ALTER TABLE games
-ADD platform_column VARCHAR(255) NOT NULL;
+ADD publisher_column VARCHAR(255) NOT NULL;
 
 ALTER TABLE games
-DROP COLUMN rpg_column;
+DROP COLUMN publisher_column;
 
 DROP TABLE games;
-
+DROP DATABASE game_company;
